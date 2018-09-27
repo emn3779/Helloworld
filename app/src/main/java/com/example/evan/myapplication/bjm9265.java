eb18a4e44b96834ae53b41c4d35504e4c3fc610e
BrianMirabito <bjm9265@g.rit.edu>
2018-09-27 15:45:16 -0400

    First commit tutorial 4

---
+++ app/src/main/java/com/example/evan/myapplication/bjm9265.java
@@ -0,0 +1,13 @@
+package com.example.evan.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class bjm9265 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bjm9265);
+    }
+}
